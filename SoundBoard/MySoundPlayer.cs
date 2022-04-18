684dd54fe3aef0a6c12284b9ff3888f70bc5a26f
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-18 21:27:35 +0200

    Startup

---
+++ SoundBoard/MySoundPlayer.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace SoundBoard
+{
+    internal class MySoundPlayer
+    {
+        public static void PlaySound(string fileName)
+        {
+            fileName = "Assets\\Animals\\" + fileName;
+            PlayFromFile(fileName);
+        }
+
+        private static async void PlayFromFile(string fileName)
+        {
+            var element = new MediaElement();
+            var folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            var file = await folder.GetFileAsync(fileName);
+            var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+            element.SetSource(stream, "");
+            element.Play();
+        }
+    }
+}
